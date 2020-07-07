f0c100e7650a8b455bf6c3d60388fcb08f923146
ljq <1137993321@qq.com>
2020-07-07 14:50:45 +0800

    分支提交

--- githello/src/cn/ljq/GitHello.java
+++ githello/src/cn/ljq/GitHello.java
@@ -4,5 +4,6 @@ public class GitHello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("I love YOU");
+        System.out.println("fhakdgakdga");
     }
 }
